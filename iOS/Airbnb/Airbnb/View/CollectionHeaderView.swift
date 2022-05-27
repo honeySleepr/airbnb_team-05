bb7b45a3d4535ec9ca97b3e9fcc2cb852f631fd1
Jason <ggj00125@gmail.com>
2022-05-27 11:09:37 +0900

    feat :sparkles:: collectionView Header 영역 구현

---
+++ iOS/Airbnb/Airbnb/View/CollectionHeaderView.swift
@@ -0,0 +1,46 @@
+//
+//  CollectionHeaderView.swift
+//  Airbnb
+//
+//  Created by Jason on 2022/05/26.
+//
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+    
+    static let identifier = "headerView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configuration()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+    
+    private var title: UILabel = {
+        let tempTitle = UILabel()
+        tempTitle.textColor = .black
+        tempTitle.textAlignment = .left
+        tempTitle.numberOfLines = 0
+        return tempTitle
+    }()
+    
+    func setHeaderText(text: String) {
+        title.text = text
+    }
+    
+    func configuration() {
+        self.addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        title.centerYAnchor.constraint(equalTo: centerYAnchor),
+        title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+        title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 16),
+        ])
+    }
+}
