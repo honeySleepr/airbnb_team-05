63bac83ba87c4cac9bf095ebc44b4ff15a148258
honeySleepr <gruzzimo@naver.com>
2022-05-29 18:34:08 +0900

    chore :adhesive_bandage:: DataProcessor TODO 추가

--- Backend/src/main/java/project/airbnb/data/DataProcessor.java
+++ Backend/src/main/java/project/airbnb/data/DataProcessor.java
@@ -53,14 +53,23 @@ public class DataProcessor {
 
 	@PostConstruct
 	public void action() {
-
+		try {
+			saveDummyBnb();
+		} catch (IOException e) {
+			log.debug("파일 경로 확인. 지정 경로 : {}", RESOURCE_PATH);
+			e.printStackTrace();
+		}
 	}
 
+	// Naver API를 이용해 주소를 위/경도로 변환하여 CSV 파일에 저장한다
+	// dummy data 용 xlsx 파일에 넣을 위/경도 정보 얻기 위해 사용
 	private void createXYCsv() throws IOException {
 		// hotel_data.xlsx 의 첫번째 시트를 사용한다
 		FileInputStream file = new FileInputStream(RESOURCE_PATH + "hotel_data.xlsx");
-		Workbook workbook = new XSSFWorkbook(file);
-		Sheet sheet = workbook.getSheetAt(0);
+		Sheet sheet;
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			sheet = workbook.getSheetAt(0);
+		}
 
 		// 위/경도 정보를 담는 리스트
 		List<String[]> coordinates = new ArrayList<>();
@@ -82,6 +91,7 @@ public class DataProcessor {
 				ApiResponse response = new RestTemplate()
 					.exchange(GEOCODE_URL + row.getCell(4).getStringCellValue(),
 						HttpMethod.GET, request, ApiResponse.class).getBody();
+
 				// API response에서 x(경도),y(위도)를 가져와서 업체명과 함께 List에 담는다
 				Coordinate coordinate = response.getCoordinates().get(0);
 				coordinates.add(new String[]{row.getCell(4).getStringCellValue(),
@@ -101,7 +111,6 @@ public class DataProcessor {
 				.map(this::convertToCSV)
 				.forEach(pw::println);
 		}
-
 	}
 
 	public String convertToCSV(String[] data) {
@@ -109,52 +118,45 @@ public class DataProcessor {
 			.collect(Collectors.joining(","));
 	}
 
-	private void insertDummyData() throws IOException {
+	private void saveDummyBnb() throws IOException {
+		// dummy data 가 저장되어 있는 hotel_data.xlsx 파일을 읽어온다
 		FileInputStream file = new FileInputStream(RESOURCE_PATH + "hotel_data.xlsx");
-		Workbook workbook = new XSSFWorkbook(file);
-		Sheet sheet = workbook.getSheetAt(0);
+		Sheet sheet;
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			// 첫번 째 시트에서 데이터를 가져온다
+			sheet = workbook.getSheetAt(0);
+		}
 		Random random = new Random();
-		for (int i = 3; i < 5; i++) { // 4행(index:3) 부터 데이터
+
+		// 데이터가 담긴 첫 행(3) 부터 N행 까지 조회하며 dummy Bnb를 생성하고 저장한다
+		for (int i = 3; i < 5; i++) {
 
 			Row row = sheet.getRow(i);
 
-			HttpHeaders headers = new HttpHeaders();
-			headers.set("X-NCP-APIGW-API-KEY-ID", clientId);
-			headers.set("X-NCP-APIGW-API-KEY", clientSecret);
-
-			HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
-
-			ApiResponse body = new RestTemplate()
-				.exchange(GEOCODE_URL + row.getCell(7).getStringCellValue(), HttpMethod.GET,
-					request, ApiResponse.class)
-				.getBody();
-
-			Coordinate coordinate = body.getCoordinates().get(0);
-
-			System.out.println("coordinate.getLatitude() = " + coordinate.getLatitude());
-			System.out.println("coordinate.getLongitude() = " + coordinate.getLongitude());
-
-			System.out.println("=====================================");
-
-			Bnb bnb = new Bnb(null, new ArrayList<>(), row.getCell(4).getStringCellValue(),
+			// [row.getCell index : data] =  1:지역1(서울시), 2:지역2(강남구), 3:업체명, 4:전체주소, 5:위도, 6:경도
+			// 저장된 값은 사용하고, 나머지 값은 랜덤하게 돌린다.
+			// TODO: 위도, 경도는 어떻게 저장하는게 좋을지 상의하기
+			Bnb bnb = new Bnb(null, new ArrayList<>(), row.getCell(3).getStringCellValue(),
 				3 + (Math.round(random.nextInt(21) * 0.1 * 10) / 10d), random.nextInt(1000),
 				new Address("한국", row.getCell(1).getStringCellValue(),
 					row.getCell(2).getStringCellValue(),
-					row.getCell(7).getStringCellValue()),
+					row.getCell(4).getStringCellValue()),
 				BnbType.values()[random.nextInt(3)], "BC",
 				2 + random.nextInt(3),
-				new Time(LocalTime.of(15, 00), LocalTime.of(11, 00)),
+				new Time(LocalTime.of(15, 0), LocalTime.of(11, 0)),
 				new BnbOption(1 + random.nextInt(3), 1 + random.nextInt(4), 1 + random.nextInt(3)),
 				"설명", 20_000 + 1000L * random.nextInt(81)
 			);
+
+			// 각 숙소당 3개의 랜덤 이미지를 저장한다. S3에 100개의 이미지를 넣고 랜덤으로 돌리고자 한다
+			// TODO: S3에 이미지 채워넣고 파일이름 번호순으로 나열하기
 			for (int j = 0; j < 3; j++) {
 				BnbImage bnbImage = new BnbImage();
 				bnbImage.setImageUrl(S3_IMAGE_URL + (1 + random.nextInt(100)));
 				bnb.saveBnbImage(bnbImage);
 			}
+
 			bnbRepository.save(bnb);
 		}
-
 	}
-
 }
