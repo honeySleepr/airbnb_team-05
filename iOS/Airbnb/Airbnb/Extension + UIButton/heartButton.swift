db5a943e3ae934515f8c58c3785231be5b8a2bc2
Jason <ggj00125@gmail.com>
2022-05-31 12:21:04 +0900

    feat :sparkles:: Heart 및 Star UIButton에 필요한 Extension 구현

---
+++ iOS/Airbnb/Airbnb/Extension + UIButton/heartButton.swift
@@ -0,0 +1,20 @@
+//
+//  heartButton.swift
+//  Airbnb
+//
+//  Created by Jason on 2022/05/31.
+//
+
+import UIKit
+
+extension UIButton {
+
+    func setImage(systemName: String) {
+        contentHorizontalAlignment = .fill
+        contentVerticalAlignment = .fill
+        
+        imageView?.contentMode = .scaleAspectFit
+        
+        setImage(UIImage(systemName: systemName), for: .normal)
+    }
+}
