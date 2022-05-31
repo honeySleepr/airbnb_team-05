78f8f932b362c432cd892edef9ed2300b031fd35
Jason <ggj00125@gmail.com>
2022-05-31 13:01:54 +0900

    feat :sparkles::TumbnailCell의 UI Layout 구현

--- iOS/Airbnb/Airbnb/View/WishlistView/TumbnailCell.swift
+++ iOS/Airbnb/Airbnb/View/WishlistView/TumbnailCell.swift
@@ -11,7 +11,8 @@ final class Tumbnail:UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        appendSubView()
+        tumbnailLayout()
     }
     
     @available(*, unavailable)
@@ -40,8 +41,36 @@ final class Tumbnail:UICollectionViewCell {
         return heart
     }()
     
+    func configure(image: UIImage?) {
+        tumbNailImage.image = image
+    }
     
+    private func appendSubView() {
+        contentView.addSubview(tumbNailImage)
+        contentView.addSubview(superHost)
+        contentView.addSubview(heart)
+    }
     
-    
-    
+    private func tumbnailLayout() {
+        tumbNailImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tumbNailImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            tumbNailImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            tumbNailImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            tumbNailImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        superHost.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            superHost.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            superHost.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16)
+        ])
+        
+        heart.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            heart.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 19),
+            heart.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -17)
+        ])
+    }
+
 }
