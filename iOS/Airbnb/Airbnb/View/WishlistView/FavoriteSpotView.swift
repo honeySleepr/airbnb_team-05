7a2ea46d896037852d656ae466bcbdf0741a9521
Jason <ggj00125@gmail.com>
2022-05-31 11:29:13 +0900

    feat :sparkles:: 위시리스트의 NavigationBar 구현

--- iOS/Airbnb/Airbnb/View/WishlistView/FavoriteSpotView.swift
+++ iOS/Airbnb/Airbnb/View/WishlistView/FavoriteSpotView.swift
@@ -7,6 +7,43 @@
 
 import UIKit
 
-class FavoriteSpotView: UIView {
+final class FavoriteSpotView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        navigationBarLayout()
+
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError()
+    }
+    
+    let navigationBar: UINavigationBar = {
+        let navigationBar = UINavigationBar()
+        let navigationItem = UINavigationItem(title: "위시리스트")
+        navigationBar.backgroundColor = .white
+        navigationBar.setItems([navigationItem], animated: true)
+        return navigationBar
+    }()
+    
+    func navigationBarLayout() {
+        let safeArea = self.safeAreaLayoutGuide
+        self.addSubview(navigationBar)
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            navigationBar.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            navigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+        
+    }
+    
+    
+    
     
 }
