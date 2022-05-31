46a4ff6664172bb5788084e506329a820cd1729b
Jason <ggj00125@gmail.com>
2022-05-31 12:24:17 +0900

    feat :sparkles:: 위시리스트 정보를 담는 InformationCell 파일 생성 및 초기설정 구현

---
+++ iOS/Airbnb/Airbnb/View/WishlistView/InformationCell.swift
@@ -0,0 +1,24 @@
+//
+//  InformationCell.swift
+//  Airbnb
+//
+//  Created by Jason on 2022/05/31.
+//
+
+import UIKit
+
+final class InformationCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError()
+    }
+    
+    
+}
