6fb37545f381e8778d38b2e16ab9d718019fb6a5
Jason <ggj00125@gmail.com>
2022-06-03 11:03:52 +0900

    feat :sparkles:: contentView의 sectionInset 및 CGSize 수정

--- iOS/Airbnb/Airbnb/View/ReservationView/ReservationView.swift
+++ iOS/Airbnb/Airbnb/View/ReservationView/ReservationView.swift
@@ -45,9 +45,9 @@ final class ReservationView: UIView {
     private lazy var collectionView: UICollectionView = {
         
         let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 20.0, right: 10.0)
+        flowLayout.sectionInset = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 0.0, right: 10.0)
         flowLayout.minimumLineSpacing = 5
-        flowLayout.itemSize = CGSize(width: 350, height: 300)
+        flowLayout.estimatedItemSize = CGSize(width: 350, height: 240)
         
         let collectionView = UICollectionView(frame:.zero, collectionViewLayout: flowLayout)
         collectionView.isScrollEnabled = true
@@ -56,6 +56,7 @@ final class ReservationView: UIView {
         
         collectionView.register(ReservationCell.self, forCellWithReuseIdentifier: ReservationCell.identifier)
         collectionView.register(CheckInOutCell.self, forCellWithReuseIdentifier: CheckInOutCell.identifier)
+        collectionView.register(SummaryInfoCell.self, forCellWithReuseIdentifier: SummaryInfoCell.identifier)
         
         return collectionView
     }()
