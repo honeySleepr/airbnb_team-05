996775001d81b4751b0ebaf826398a16464a663c
Jason <ggj00125@gmail.com>
2022-06-10 03:23:38 +0900

    feat :sparkles:: locationViewController Mapkit 관련 내용 구현

--- iOS/Airbnb/Airbnb/ViewController/LocationTableViewController.swift
+++ iOS/Airbnb/Airbnb/ViewController/LocationTableViewController.swift
@@ -10,9 +10,56 @@ import MapKit
 
 final class LocationTableViewController: UIViewController {
     
+    private var searchCompleter = MKLocalSearchCompleter()      // 검색을 도와주는 변수
+    private var searchResults = [MKLocalSearchCompletion]()     // 검색 결과를 담는 변수
+    private var searchRegion = MKCoordinateRegion(MKMapRect.world) // 검색 지역 범위를 결정하는 변수
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        setupSearchCompleter()
+    }
+    
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "어디로 여행가세요?"
+        return searchBar
+    }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .yellow
+        return tableView
+    }()
+    
+    func setupLocationTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(LocationViewCell.self, forCellReuseIdentifier: LocationViewCell.identifier)
+        tableView.separatorStyle = .none
+    }
+    
+}
+
+extension LocationTableViewController: MKLocalSearchCompleterDelegate {
+    
+    func setupSearchCompleter() {
+        searchCompleter.delegate = self
+        searchCompleter.resultTypes = .address
+        searchCompleter.region = searchRegion
+    }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        // locationView.tableView.reloadData()
+    }
+}
+
+extension LocationTableViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchCompleter.queryFragment = searchText
     }
     
 }
