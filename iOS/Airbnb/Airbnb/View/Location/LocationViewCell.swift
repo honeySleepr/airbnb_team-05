d353d877175bd03c9bd7d3f99b2ada8572372f11
Jason <ggj00125@gmail.com>
2022-06-10 03:05:51 +0900

    feat :sparkles:: LocationViewCell 구현

--- iOS/Airbnb/Airbnb/View/Location/LocationViewCell.swift
+++ iOS/Airbnb/Airbnb/View/Location/LocationViewCell.swift
@@ -5,4 +5,57 @@
 //  Created by Jason on 2022/06/10.
 //
 
-import Foundation
+import UIKit
+
+final class LocationViewCell: UITableViewCell {
+    
+    static let identifier = "LocationViewCell"
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        appendSubView()
+        setupLocationCellLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private let locationImage: UIImageView = {
+        let locationImage = UIImageView()
+        locationImage.image = UIImage(named: "cityBox")
+        return locationImage
+    }()
+    
+    private let locationTitle: UILabel = {
+        let locationTitle = UILabel()
+        locationTitle.textColor = .systemGray
+        locationTitle.font = .systemFont(ofSize: 17)
+        return locationTitle
+    }()
+    
+    private func appendSubView() {
+        contentView.addSubview(locationImage)
+        contentView.addSubview(locationTitle)
+    }
+    
+    private func setupLocationCellLayout() {
+        locationImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            locationImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            locationImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            locationImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ])
+        
+        locationTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            locationTitle.leadingAnchor.constraint(equalTo: locationImage.trailingAnchor, constant: 20),
+            locationTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ])
+    }
+}
